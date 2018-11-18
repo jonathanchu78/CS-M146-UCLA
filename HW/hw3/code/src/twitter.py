7ae711ca6c43e08fd035dfbfed1b7f40a17bba05
Jonathan <jonathanchu78@gmail.com>
2018-11-18 14:40:13 -0800

    hw3 done

--- HW/hw3/code/src/twitter.py
+++ HW/hw3/code/src/twitter.py
@@ -6,11 +6,12 @@ Description : Twitter
 from string import punctuation
 
 import numpy as np
+import pprint
 
 # !!! MAKE SURE TO USE SVC.decision_function(X), NOT SVC.predict(X) !!!
 # (this makes ``continuous-valued'' predictions)
 from sklearn.svm import SVC
-from sklearn.cross_validation import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
 
 ######################################################################
@@ -20,11 +21,11 @@ from sklearn import metrics
 def read_vector_file(fname):
     """
     Reads and returns a vector from a file.
-    
+
     Parameters
     --------------------
         fname  -- string, filename
-        
+
     Returns
     --------------------
         labels -- numpy array of shape (n,)
@@ -41,16 +42,16 @@ def extract_words(input_string):
     """
     Processes the input_string, separating it into "words" based on the presence
     of spaces, and separating punctuation marks into their own words.
-    
+
     Parameters
     --------------------
         input_string -- string of characters
-    
+
     Returns
     --------------------
         words        -- list of lowercase "words"
     """
-    
+
     for c in punctuation :
         input_string = input_string.replace(c, ' ' + c + ' ')
     return input_string.lower().split()
@@ -60,21 +61,26 @@ def extract_dictionary(infile):
     """
     Given a filename, reads the text file and builds a dictionary of unique
     words/punctuations.
-    
+
     Parameters
     --------------------
         infile    -- string, filename
-    
+
     Returns
     --------------------
         word_list -- dictionary, (key, value) pairs are (word, index)
     """
-    
+
     word_list = {}
     with open(infile, 'rU') as fid :
         ### ========== TODO : START ========== ###
         # part 1a: process each line to populate word_list
-        pass
+        counter = 0
+        for line in fid:
+            for word in extract_words(line):
+                if word not in word_list:
+                    word_list[word] = counter
+                    counter += 1
         ### ========== TODO : END ========== ###
 
     return word_list
@@ -84,12 +90,12 @@ def extract_feature_vectors(infile, word_list):
     """
     Produces a bag-of-words representation of a text file specified by the
     filename infile based on the dictionary word_list.
-    
+
     Parameters
     --------------------
         infile         -- string, filename
         word_list      -- dictionary, (key, value) pairs are (word, index)
-    
+
     Returns
     --------------------
         feature_matrix -- numpy array of shape (n,d)
@@ -97,17 +103,23 @@ def extract_feature_vectors(infile, word_list):
                             n is the number of non-blank lines in the text file
                             d is the number of unique words in the text file
     """
-    
+
     num_lines = sum(1 for line in open(infile,'rU'))
     num_words = len(word_list)
     feature_matrix = np.zeros((num_lines, num_words))
-    
+
     with open(infile, 'rU') as fid :
         ### ========== TODO : START ========== ###
         # part 1b: process each line to populate feature_matrix
-        pass
+        line_num = 0
+        for line in fid:
+            for word in extract_words(line):
+                feature_matrix[line_num][word_list[word]] = 1
+            line_num += 1
+        # print feature_matrix
+        # print np.size(feature_matrix)
         ### ========== TODO : END ========== ###
-        
+
     return feature_matrix
 
 
@@ -117,16 +129,16 @@ def extract_feature_vectors(infile, word_list):
 
 def performance(y_true, y_pred, metric="accuracy"):
     """
-    Calculates the performance metric based on the agreement between the 
+    Calculates the performance metric based on the agreement between the
     true labels and the predicted labels.
-    
+
     Parameters
     --------------------
         y_true -- numpy array of shape (n,), known labels
         y_pred -- numpy array of shape (n,), (continuous-valued) predictions
         metric -- string, option used to select the performance measure
-                  options: 'accuracy', 'f1-score', 'auroc'       
-    
+                  options: 'accuracy', 'f1-score', 'auroc'
+
     Returns
     --------------------
         score  -- float, performance score
@@ -134,10 +146,21 @@ def performance(y_true, y_pred, metric="accuracy"):
     # map continuous-valued predictions to binary labels
     y_label = np.sign(y_pred)
     y_label[y_label==0] = 1
-    
+
     ### ========== TODO : START ========== ###
     # part 2a: compute classifier performance
-    return 0
+    score = 0
+
+    if metric == "accuracy":
+        score = metrics.accuracy_score(y_true, y_label)
+
+    if metric == "f1-score":
+        score = metrics.f1_score(y_true, y_label)
+
+    if metric == "auroc":
+        score = metrics.roc_auc_score(y_true, y_label)
+
+    return score
     ### ========== TODO : END ========== ###
 
 
@@ -147,7 +170,7 @@ def cv_performance(clf, X, y, kf, metric="accuracy"):
     Trains classifier on k-1 folds and tests on the remaining fold.
     Calculates the k-fold cross-validation performance metric for classifier
     by averaging the performance across folds.
-    
+
     Parameters
     --------------------
         clf    -- classifier (instance of SVC)
@@ -157,15 +180,27 @@ def cv_performance(clf, X, y, kf, metric="accuracy"):
         y      -- numpy array of shape (n,), binary labels {1,-1}
         kf     -- cross_validation.KFold or cross_validation.StratifiedKFold
         metric -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         score   -- float, average cross-validation performance across k folds
     """
-    
+
     ### ========== TODO : START ========== ###
-    # part 2b: compute average cross-validation performance    
-    return 0
+    # part 2b: compute average cross-validation performance
+    score = 0
+    counter = 0
+    for train_index, test_index in kf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        clf.fit(X_train, y_train)
+        # print y_test
+        # print clf.decision_function(X_test)
+        score += performance(y_test, clf.decision_function(X_test), metric)
+        counter += 1
+
+
+    return score / counter
     ### ========== TODO : END ========== ###
 
 
@@ -174,7 +209,7 @@ def select_param_linear(X, y, kf, metric="accuracy"):
     Sweeps different settings for the hyperparameter of a linear-kernel SVM,
     calculating the k-fold CV performance for each setting, then selecting the
     hyperparameter that 'maximize' the average k-fold CV performance.
-    
+
     Parameters
     --------------------
         X      -- numpy array of shape (n,d), feature vectors
@@ -183,18 +218,25 @@ def select_param_linear(X, y, kf, metric="accuracy"):
         y      -- numpy array of shape (n,), binary labels {1,-1}
         kf     -- cross_validation.KFold or cross_validation.StratifiedKFold
         metric -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         C -- float, optimal parameter value for linear-kernel SVM
     """
-    
+
     print 'Linear SVM Hyperparameter Selection based on ' + str(metric) + ':'
     C_range = 10.0 ** np.arange(-3, 3)
-    
+
     ### ========== TODO : START ========== ###
     # part 2: select optimal hyperparameter using cross-validation
-    return 1.0
+    scores = {}
+    for C in C_range:
+        score = cv_performance(SVC(kernel='linear', C=C), X, y, kf, metric)
+        scores[C] = score
+    pp = pprint.PrettyPrinter(indent = 3)
+    pp.pprint(scores)
+
+    return max(scores, key=scores.get)
     ### ========== TODO : END ========== ###
 
 
@@ -202,7 +244,7 @@ def select_param_linear(X, y, kf, metric="accuracy"):
 def performance_test(clf, X, y, metric="accuracy"):
     """
     Estimates the performance of the classifier using the 95% CI.
-    
+
     Parameters
     --------------------
         clf          -- classifier (instance of SVC)
@@ -212,7 +254,7 @@ def performance_test(clf, X, y, metric="accuracy"):
                           d = number of features
         y            -- numpy array of shape (n,), binary labels {1,-1} of test set
         metric       -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         score        -- float, classifier performance
@@ -220,39 +262,51 @@ def performance_test(clf, X, y, metric="accuracy"):
 
     ### ========== TODO : START ========== ###
     # part 3: return performance on test data by first computing predictions and then calling performance
-
-    score = 0        
-    return score
+    y_pred = clf.decision_function(X)
+    return performance(y, y_pred, metric)
     ### ========== TODO : END ========== ###
 
 
 ######################################################################
 # main
 ######################################################################
- 
+
 def main() :
     np.random.seed(1234)
-    
-    # read the tweets and its labels   
+
+    # read the tweets and its labels
     dictionary = extract_dictionary('../data/tweets.txt')
     X = extract_feature_vectors('../data/tweets.txt', dictionary)
     y = read_vector_file('../data/labels.txt')
-    
+
     metric_list = ["accuracy", "f1_score", "auroc"]
-    
+
     ### ========== TODO : START ========== ###
     # part 1: split data into training (training + cross-validation) and testing set
-    
+    X_train = X[0:560]
+    X_test = X[560:630]
+    y_train = y[0:560]
+    y_test = y[560:630]
+
     # part 2: create stratified folds (5-fold CV)
-    
+    kf = StratifiedKFold(n_splits=5)
+    # print cv_performance(SVC(kernel='linear'), X_train, y_train, kf)
+
     # part 2: for each metric, select optimal hyperparameter for linear-kernel SVM using CV
-        
+    metrics = ['accuracy', 'f1-score', 'auroc']
+    # for metric in metrics:
+    #     print 'With the %s metric, the best value of C is %f' % (metric, select_param_linear(X_train, y_train, kf, metric=metric))
+
     # part 3: train linear-kernel SVMs with selected hyperparameters
-    
+    clf = SVC(kernel='linear', C=100)
+    clf.fit(X_train, y_train)
+    for metric in metrics:
+        print 'With C = 100, the %s metric reports a score of %f' %(metric, performance_test(clf, X_test, y_test, metric))
+
     # part 3: report performance on test data
-    
+
     ### ========== TODO : END ========== ###
-    
-    
+
+
 if __name__ == "__main__" :
     main()
